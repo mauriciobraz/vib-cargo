3011f63e24a57aec5c3d80a1b5c81bb0eec4d59e
Maurício Braz <197079527+mauricioftw@users.noreply.github.com>
2025-09-27 18:54:24 -0400

    plugin src

--- plugin.go
+++ plugin.go
@@ -1,98 +1,102 @@
 package main
 
 import (
-	"fmt"
 	"C"
 	"encoding/json"
+	"fmt"
+	"strings"
+
 	"github.com/vanilla-os/vib/api"
 )
 
-type ExampleModule struct {
-	// Mandatory values, your plugin will not work if these are not present!
-	Name string `json:"name"`
-	Type string `json:"type"`
+type CargoModule struct {
+	Name   string     `json:"name"`
+	Type   string     `json:"type"`
+	Source api.Source `json:"source"`
 
-	// Additional values such as Source can be added here
-	Source api.Source `json:"Source"`
+	Release   bool `json:"release"`
+	NoDefault bool `json:"no-default"`
+
+	InstallPath string   `json:"install-path"`
+	Features    []string `json:"features"`
+	BuildFlags  []string `json:"build-flags"`
 }
 
-// Plugins can define extra functions that are used internally
-// Vib will never call any function other than BuildModule
-func fetchSources(source api.Source, name string, recipe *api.Recipe) error {
-	// The plugin api offers functions to download sources
-	// To be able to use them, the use of api.Source for
-	// source definition is recommended
-	// Using these functions to fetch sources is not required
-	// but highly recommended to ensure sources are always in the right directory
-	err := api.DownloadSource(recipe.DownloadsPath, source, name)
-	if err != nil {
+func fetchSource(source api.Source, name string, recipe *api.Recipe) error {
+	if err := api.DownloadSource(recipe, source, name); err != nil {
 		return err
 	}
-	err = api.MoveSource(recipe.DownloadsPath, recipe.SourcesPath, source, name)
-	return err
+	return api.MoveSource(recipe.DownloadsPath, recipe.SourcesPath, source, name)
 }
 
-
-// This function returns information about the plugin, most notably the type of plugin.
-// Plugins that do not define this function are considered deprecated
-// while they still work, support may be dropped in future releases.
 //export PlugInfo
 func PlugInfo() *C.char {
-	plugininfo := &api.PluginInfo{
-		Name: "PLUGINAME", // The name of the plugin
-		Type: api.BuildPlugin, // The type of plugin. This plugin template does NOT function as a FinalizePlugin, so unless you have manually modified it accordingly, this value should stay as api.BuildPlugin
-		UseContainerCmds: False, // If the plugin adds its own Containerfile directives, set it to False if the plugin only generates shell commands
+	info := &api.PluginInfo{
+		Name:             "cargo",
+		Type:             api.BuildPlugin,
+		UseContainerCmds: false,
 	}
-	pluginjson, err := json.Marshal(plugininfo)
+	data, err := json.Marshal(info)
 	if err != nil {
 		return C.CString(fmt.Sprintf("ERROR: %s", err.Error()))
 	}
-	return C.CString(string(pluginjson))
+	return C.CString(string(data))
 }
-
-// This is the entry point for plugins that vib calls
-// The arguments are required to be (*C.char, *C.char) => string
-// Make sure NOT to remove the "export BuildModule"
 
 //export BuildModule
-func BuildModule(moduleInterface *C.char, recipeInterface *C.char, arch *C.char) *C.Char {
-	// It is advisable to convert the interface to an actual struct
-	// The use of json.Unmarshal for this is recommended, but not required
-	var module *ExampleModule
+func BuildModule(moduleInterface *C.char, recipeInterface *C.char, arch *C.char) *C.char {
+	var module *CargoModule
 	var recipe *api.Recipe
-	
-	err := json.Unmarshal([]byte(C.GoString(moduleInterface)), &module)
-	if err != nil {
 
-		// Due to the way c-shared builds work, the function has to return a CString
-		// CGO includes a proper way to ensure strings are converted to CStrings
-		// Any return in BuildModule will have to be wrapped with C.CString
-		//
-		// Since only one value can be returned, vib checks if the return
-		// starts with "ERROR:", if this is the case, it assumes that the
-		// plugin has failed and takes anything after the "ERROR:" as the
-		// error message.
-		return C.CString(fmt.Sprintf("ERROR: %s", err.Error()))
-	}
-	
-	err = json.Unmarshal([]byte(C.GoString(recipeInterface)), &recipe)
-	if err != nil {
-		return C.CString(fmt.Sprintf("ERROR: %s", err.Error()))
-	}
-	
-	err = fetchSources(module.Source, module.Name, recipe)
-	if err != nil {
+	if err := json.Unmarshal([]byte(C.GoString(moduleInterface)), &module); err != nil {
 		return C.CString(fmt.Sprintf("ERROR: %s", err.Error()))
 	}
 
-	// The sources will be made available at /sources/ during build
-	// if the plugins requires manually downloaded sources, they will
-	// be available in /sources/<modulename>
-	cmd := fmt.Sprintf("cd /sources/%s && cp * /etc/%s", module.Name, module.Name)
+	if err := json.Unmarshal([]byte(C.GoString(recipeInterface)), &recipe); err != nil {
+		return C.CString(fmt.Sprintf("ERROR: %s", err.Error()))
+	}
 
-	return C.CString(cmd)
+	if !api.TestArch(module.Source.OnlyArches, C.GoString(arch)) {
+		return C.CString("")
+	}
+
+	if err := fetchSource(module.Source, module.Name, recipe); err != nil {
+		return C.CString(fmt.Sprintf("ERROR: %s", err.Error()))
+	}
+
+	workDir := fmt.Sprintf("/sources/%s", api.GetSourcePath(module.Source, module.Name))
+	installPath := module.InstallPath
+	if installPath == "" {
+		installPath = "/usr/bin"
+	}
+
+	cargoCmd := "cargo build --verbose"
+	if module.Release {
+		cargoCmd += " --release"
+	}
+	if len(module.Features) > 0 {
+		cargoCmd += " --features " + strings.Join(module.Features, ",")
+	}
+	if module.NoDefault {
+		cargoCmd += " --no-default-features"
+	}
+	if len(module.BuildFlags) > 0 {
+		cargoCmd += " " + strings.Join(module.BuildFlags, " ")
+	}
+
+	binarySubdir := "debug"
+	if module.Release {
+		binarySubdir = "release"
+	}
+
+	binaryPath := fmt.Sprintf("target/%s/%s", binarySubdir, module.Name)
+
+	fullCmd := fmt.Sprintf(
+		"cd %s && if ! command -v cargo >/dev/null 2>&1; then echo 'installing cargo...' >&2 && apt-get update && apt-get install -y cargo rustc; fi && %s && cp %s %s/ && chmod +x %s/%s",
+		workDir, cargoCmd, binaryPath, installPath, installPath, module.Name,
+	)
+
+	return C.CString(fullCmd)
 }
 
-// Keep this function empty
-// It will never be triggered. Any code in here is dead code.
 func main() {}
